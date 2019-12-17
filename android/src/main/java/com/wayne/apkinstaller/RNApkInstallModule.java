4b88b27cfd5b8473457f348761f78987b85b16d1
Jackie <zhengjiekun@gmail.com>
2019-12-17 14:47:12 +0800

    pass provider from rn

--- android/src/main/java/com/wayne/apkinstaller/RNApkInstallModule.java
+++ android/src/main/java/com/wayne/apkinstaller/RNApkInstallModule.java
@@ -43,7 +43,7 @@ public class RNApkInstallModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void install(String path) {
+  public void install(String path, String provider) {
     String cmd = "chmod 777 " +path;
     try {
       Runtime.getRuntime().exec(cmd);
@@ -57,7 +57,7 @@ public class RNApkInstallModule extends ReactContextBaseJavaModule {
     if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.N) { //判读版本是否在7.0以上
       //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
       Uri apkUri =
-              FileProvider.getUriForFile(_context, "com.huaxing.alpha.apps.apollo.FileProvider", file);
+              FileProvider.getUriForFile(_context, provider, file);
       //添加这一句表示对目标应用临时授权该Uri所代表的文件
       intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
       intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
